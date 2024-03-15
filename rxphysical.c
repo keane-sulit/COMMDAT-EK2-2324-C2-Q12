dbafc9186bffb4993af703d9e01b298b73afd0f7
EzraExodia1 <philip_martin_escamilla@dlsu.edu.ph>
2024-03-15 23:01:40 +0800

    Uploaded Physical Layer Source Code

---
+++ rxphysical.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_BITS 1000
+
+// Function to convert integer signal to binary
+void signalToBinary(int* input, int len, char* binary) {
+    int binaryIndex = 0;
+    int i;
+
+    for (i = 0; i < len; i++) {
+        if (input[i] == 5) {
+            binary[binaryIndex++] = '1';
+        } else if (input[i] == -5) {
+            binary[binaryIndex++] = '1';
+        } else {
+            binary[binaryIndex++] = '0';
+        }
+    }
+    binary[binaryIndex] = '\0';
+}
+
+// Function to convert binary to hexadecimal
+char* binaryToHex(char* binary) {
+    int len = strlen(binary);
+    char* hex = (char*)malloc((len / 4 + 1) * sizeof(char));
+    int i, j;
+
+    // Padding with zeroes if necessary
+    while (len % 4 != 0) {
+        binary[len] = '0';
+        len++;
+    }
+    binary[len] = '\0';
+
+    // Conversion to hexadecimal
+    for (i = 0; i < len / 4; i++) {
+        int val = 0;
+        for (j = 0; j < 4; j++) {
+            val = val * 2 + (binary[i * 4 + j] - '0');
+        }
+        if (val < 10)
+            hex[i] = val + '0';
+        else
+            hex[i] = val - 10 + 'A';
+    }
+    hex[i] = '\0';
+    return hex;
+}
+
+int main() {
+    int* input = NULL;
+    int capacity = 0;
+    int len = 0;
+    int num;
+
+    // Read piped integer input
+    while (scanf("%d", &num) == 1) {
+        if (len >= capacity) {
+            capacity = (capacity == 0) ? 1 : capacity * 2;
+            int* temp = realloc(input, capacity * sizeof(int));
+            if (temp == NULL) {
+                printf("Memory allocation failed\n");
+                if (input != NULL) free(input);
+                return 1;
+            }
+            input = temp;
+        }
+        input[len++] = num;
+    }
+
+    char binary[MAX_BITS];
+    signalToBinary(input, len, binary);
+
+    char* hexadecimal = binaryToHex(binary);
+    printf("%s\n", hexadecimal);
+
+    free(input);
+    free(hexadecimal);
+
+    return 0;
+}
+
---
+++ txphysical.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Function to convert hexadecimal to binary
+void hex_to_bin(char hex_digit[], char binary_string[], int count) {
+    int i = 0;
+    count = 0;
+    while (hex_digit[i]) {
+        switch(hex_digit[i]) {
+            case '0':
+                strcat(binary_string, "0000");
+                break;
+            case '1':
+                strcat(binary_string, "0001");
+                break;
+            case '2':
+                strcat(binary_string, "0010");
+                break;
+            case '3':
+                strcat(binary_string, "0011");
+                break;
+            case '4':
+                strcat(binary_string, "0100");
+                break;
+            case '5':
+                strcat(binary_string, "0101");
+                break;
+            case '6':
+                strcat(binary_string, "0110");
+                break;
+            case '7':
+                strcat(binary_string, "0111");
+                break;
+            case '8':
+                strcat(binary_string, "1000");
+                break;
+            case '9':
+                strcat(binary_string, "1001");
+                break;
+            case 'A':
+            case 'a':
+                strcat(binary_string, "1010");
+                break;
+            case 'B':
+            case 'b':
+                strcat(binary_string, "1011");
+                break;
+            case 'C':
+            case 'c':
+                strcat(binary_string, "1100");
+                break;
+            case 'D':
+            case 'd':
+                strcat(binary_string, "1101");
+                break;
+            case 'E':
+            case 'e':
+                strcat(binary_string, "1110");
+                break;
+            case 'F':
+            case 'f':
+                strcat(binary_string, "1111");
+                break;
+            default:
+                printf("\nInvalid hexadecimal digit %c", hex_digit[i]);
+                return;
+        }
+        i++;
+    }
+}
+
+
+// Function to encode a binary string using bipolar AMI encoding
+void txphysical(const char *binary_string, int *encodedOutput, int length) {
+    int lastVoltage = 5;  int i;// Start with +5V for the first '1'
+    for (i = 0; i < length; i++) {
+        if (binary_string[i] == '1') {
+            encodedOutput[i] = lastVoltage;
+            // Alternate the voltage for the next '1'
+            // lastVoltage = (lastVoltage == 5) ? -5 : 5;
+            // or
+            if (lastVoltage == 5) {
+                lastVoltage = -5;
+            } else {
+                lastVoltage = 5;
+            }
+        } else {
+            // '0' is represented by 0V
+            encodedOutput[i] = 0;
+        }
+    }
+}
+
+// Main function
+int main() {
+    char hex_digit[1024];  // Buffer to hold the user input, adjust size as needed
+	char binary_string[1024];
+	int count;
+    scanf("%s", hex_digit);  // Read the hex string from datalink layer
+    int i;
+
+	hex_to_bin(hex_digit, binary_string, count);
+	
+	int length = strlen(binary_string);
+    int *encodedSignal = (int *)malloc(length * sizeof(int));
+ 
+    if (encodedSignal == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;  // Return with error code
+    }
+    /*txphysical(binary_string, encodedSignal, length);
+    printf("\n");
+    printf("%s", binary_string);
+    printf("\n");*/
+    for (i = 0; i < length; i++) {
+        printf("%d ", encodedSignal[i]);
+    }
+    printf("\n");
+
+    free(encodedSignal);
+    return 0;
+}
